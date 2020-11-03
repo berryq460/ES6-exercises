fc96d59d7e9872a20ef6df9674b2ffc9969f7dac
Quiana Berry <berry.quiana@gmail.com>
2020-11-02 22:32:59 -0500

    classwork 11/2

--- main.js
+++ main.js
@@ -5,6 +5,11 @@
 // array with numbers that contains all of the evens and odds
 
 
+let evens =  [2,4,6,8,10]
+let odds  =  [1,3,6,9,11]
+arr = [...evens,...odds];
+console.log(arr)
+  
 
 //Exercise #2
 // Define an object favoriteFoods that has keys representing three of 
@@ -15,7 +20,10 @@
 // Use the spread operator to build a final allFoods with the 
 // items from the two objects you just built
 
-
+faveFoods = { burgers: 'Qdoba', pizza: 'Elsies', steak: 'Harlem Steakhouse' }
+friendFaveFoods = { burrito: 'chipotle', pasta: 'carmines', salad: 'chopped' }
+allFoods = { ...faveFoods, ...friendFaveFoods };
+console.log(allFoods);
 
 // Exercise #3
 // Define your favorite movie character in an object
@@ -30,3 +38,18 @@
 // 2) define three variables (age, breed, name) outside of the object
 // 3) add those variables to your pet
 // log the most important thing about it: the pet's name 
+
+myself = { name: 'Jonathan', age: '26', hometown: 'Queens' }
+// const { name, age, hometown } = myself
+// console.log(name, age, hometown);
+const { name, age, ...me } = myself
+console.log(name, age, me.hometown);
+const pet = {}
+myself = { ...myself, pet }
+let petName = 'fred'
+let petAge = 1
+let petBreed = 'German-Shephard'
+myself.pet.Name = petName
+myself.pet.Age = petAge
+myself.pet.Breed = petBreed
+console.log(myself.pet.Name)
